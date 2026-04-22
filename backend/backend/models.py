5d0aada9ece72784cd6542120816e876bffcd3b5
r0n3x <isa.dz@web.de>
2026-04-22 14:27:29 +0200

    models.py

---
+++ backend/backend/models.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+
+class ClassifyRequestModel(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    description: str
+    hs_code: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+class CustomsValueRequestModel(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    price: float
+    currency: str
+    freight: float
+    insurance: float
+    price_eur: float
+    customs_value: float
+    created_at: datetime = Field(default_factory=datetime.utcnow)
